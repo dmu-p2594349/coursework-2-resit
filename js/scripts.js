d93f68b5bbe7cf68dbd2354e1aa244c5704242a8
Bertie Nichols <p2594349@my365.dmu.ac.uk>
2021-07-25 15:36:24 +0100

    Basic form + JS function to process

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-function sayHello() {
-  const name = document.getElementById('nameField').value
-  alert(`Hello ${name}`)
+function submitClipOfTheWeek() {
+  const name = document.getElementById('clipOfTheWeek-fullName').value
+  const ride = document.getElementById('clipOfTheWeek-ride').value
+  const url = document.getElementById('clipOfTheWeek-url').value
+  console.log('POST to a server somewhere')
+  console.log('Values from the form are ', {name, ride, url})
+  alert('Thanks, I hate it')
 }
